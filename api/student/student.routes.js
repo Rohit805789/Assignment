d318414c1675369a800f6afd586d8494510ce1eb
Rohit kumar <102985579+Rohit805789@users.noreply.github.com>
2022-10-26 16:24:22 +0530

    Update student.routes.js

--- api/student/student.routes.js
+++ api/student/student.routes.js
@@ -22,6 +22,6 @@ const upload = multer({ storage: storage })    // used  for file uploading
 
 router.post('/upload',upload.single('studentfile'),newStudentInsert)  // use for csv file upload 
 router.get('/students/:id',getStudentDetails)   //  get student data acc to id 
-router.get('/students',getAllStudentResults) // get result all student
+router.get('/students',getAllStudentResults) // get result all student enter the passing mark ?resultStatus=
 
 module.exports = router
